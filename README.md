5c6fe05ffbf6b34848853b5f00bb241c56aee04b
Lars Reed <lars@kalars.net>
2021-11-21 22:32:14 +0100

    hints/Main

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javazone2021
 Innhold til JavaZone 2021
+Et par 
+hint kan ses i [Main.java](src/main/java/no/toll/javazone/hints/Main.java)
---
+++ src/main/java/no/toll/javazone/hints/Main.java
@@ -0,0 +1,61 @@
+package no.toll.javazone.hints;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String[] what = new String[] {
+                javaWasOriginallyDevelopedByThisPersonAtSunMicrosystems(),
+                nonBlockingCompletableFutureAndAjaxAreKeywordsThatDescribesAProgrammingParadigm(),
+                aPieceOfMemoryThatCanContainADataValue(),
+                sheHasBeenCreditedAsTheFirstComputerProgrammer(),
+                aNumberThatIsActuallyNothingWrittenAsAWord(),
+                simulaDevelopedInNorwayInThe1960IsConsideredTheFirstProgrammingLanguageOfThisParadigm(),
+                aLightweightHighPerformanceJavaScriptRuntimeOnTheJvmAndAlsoSharingItsNameWithAnAnimal(),
+                anEventThatOccursDuringTheExecutionOfAProgramThatDisruptsTheNormalFlowOfInstructions()
+        };
+        final String solution =
+                Arrays.stream(what).map(s -> s.substring(0, 1)).collect(Collectors.joining())
+                      .concat(" ")
+                      .concat(theYearJavaSe16AndJavaSe17WasReleased());
+        System.out.println(solution);
+    }
+
+    private static String javaWasOriginallyDevelopedByThisPersonAtSunMicrosystems() {
+        return "James Gosling";
+    }
+
+    private static String nonBlockingCompletableFutureAndAjaxAreKeywordsThatDescribesAProgrammingParadigm() {
+        return "asynchronous programming";
+    }
+
+    private static String aPieceOfMemoryThatCanContainADataValue() {
+        return "variable";
+    }
+
+    private static String sheHasBeenCreditedAsTheFirstComputerProgrammer() {
+        return "Ada Lovelace".toLowerCase();
+    }
+
+    private static String aNumberThatIsActuallyNothingWrittenAsAWord() {
+        return "zero".toUpperCase();
+    }
+
+    private static String simulaDevelopedInNorwayInThe1960IsConsideredTheFirstProgrammingLanguageOfThisParadigm() {
+        return "object-oriented programming";
+    }
+
+    private static String aLightweightHighPerformanceJavaScriptRuntimeOnTheJvmAndAlsoSharingItsNameWithAnAnimal() {
+        return "Nashorn".toLowerCase();
+    }
+
+    private static String anEventThatOccursDuringTheExecutionOfAProgramThatDisruptsTheNormalFlowOfInstructions() {
+        return "exception";
+    }
+
+    private static String theYearJavaSe16AndJavaSe17WasReleased() {
+        return "2021";
+    }
+}
